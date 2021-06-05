96c565ffe64ba4786f74f3c6c6ecf527c09d19cd
masummim50 <33899137+masummim50@users.noreply.github.com>
2021-06-05 10:03:16 +0600

    changed header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,15 @@
-import { faBookOpen, faEnvelope, faFileAlt, faHome, faProjectDiagram, faUser } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {
+  faBookOpen,
+  faEnvelope,
+  faFileAlt,
+  faHome,
+  faProjectDiagram,
+  faUser
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
@@ -11,30 +19,98 @@ const Header = () => {
           <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container-fluid">
               <a class="navbar-brand w-25" href="#">
-                <img class="w-75" src="icons/logo.png" alt=""/>
+                <img class="w-75" src="icons/logo.png" alt="" />
               </a>
-              <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <button
+                class="navbar-toggler bg-white"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
                 <span class="navbar-toggler-icon bg-white"></span>
               </button>
-              <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <div
+                class="myHeaderStyle collapse navbar-collapse justify-content-end"
+                id="navbarNav"
+              >
                 <ul class="navbar-nav my-nav-style">
                   <li class="nav-item ms-1">
-                    <NavLink to='/' exact={true} className="nav-link text-white" activeClassName="active"><span><FontAwesomeIcon icon={faHome}></FontAwesomeIcon></span> Home</NavLink>
+                    <NavLink
+                      to="/"
+                      exact={true}
+                      className="nav-link text-white"
+                      activeClassName="active"
+                    >
+                      <span>
+                        <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
+                      </span>{" "}
+                      Home
+                    </NavLink>
                   </li>
                   <li class="nav-item ms-1">
-                  <NavLink to='/about' className="nav-link text-white" activeClassName="active"><span><FontAwesomeIcon icon={faUser}></FontAwesomeIcon></span> About Me</NavLink>
+                    <NavLink
+                      to="/about"
+                      className="nav-link text-white"
+                      activeClassName="active"
+                    >
+                      <span>
+                        <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+                      </span>{" "}
+                      About Me
+                    </NavLink>
                   </li>
                   <li class="nav-item ms-1">
-                  <NavLink to='/resume' className="nav-link text-white" activeClassName="active"><span><FontAwesomeIcon icon={faFileAlt}></FontAwesomeIcon></span> Resume</NavLink>
+                    <NavLink
+                      to="/resume"
+                      className="nav-link text-white"
+                      activeClassName="active"
+                    >
+                      <span>
+                        <FontAwesomeIcon icon={faFileAlt}></FontAwesomeIcon>
+                      </span>{" "}
+                      Resume
+                    </NavLink>
                   </li>
                   <li class="nav-item ms-1">
-                  <NavLink to='/projects' className="nav-link text-white" activeClassName="active"><span><FontAwesomeIcon icon={faProjectDiagram}></FontAwesomeIcon></span> Projects</NavLink>
+                    <NavLink
+                      to="/projects"
+                      className="nav-link text-white"
+                      activeClassName="active"
+                    >
+                      <span>
+                        <FontAwesomeIcon
+                          icon={faProjectDiagram}
+                        ></FontAwesomeIcon>
+                      </span>{" "}
+                      Projects
+                    </NavLink>
                   </li>
                   <li class="nav-item ms-1">
-                  <NavLink to='/blog' className="nav-link text-white" activeClassName="active"><span><FontAwesomeIcon icon={faBookOpen}></FontAwesomeIcon></span> Blog</NavLink>
+                    <NavLink
+                      to="/blog"
+                      className="nav-link text-white"
+                      activeClassName="active"
+                    >
+                      <span>
+                        <FontAwesomeIcon icon={faBookOpen}></FontAwesomeIcon>
+                      </span>{" "}
+                      Blog
+                    </NavLink>
                   </li>
                   <li class="nav-item ms-1">
-                  <NavLink to='/contact' className="nav-link text-white" activeClassName="active"><span><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></span> Contact</NavLink>
+                    <NavLink
+                      to="/contact"
+                      className="nav-link text-white"
+                      activeClassName="active"
+                    >
+                      <span>
+                        <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+                      </span>{" "}
+                      Contact
+                    </NavLink>
                   </li>
                 </ul>
               </div>
