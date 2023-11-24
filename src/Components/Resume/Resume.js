52245a3cf7552f773b91a17500fff1719791aa5a
vchauha4 <vchauha4@uwo.ca>
2023-11-23 23:56:08 -0500

    Update Resume.js
    resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -21,15 +21,44 @@ const Resume = () => {
             <h3 className="Experience">Experience:</h3>
             <br></br>
             <h5>Data Engineer - Quantexa</h5>
+            <p>In my role as a Data Engineer I have expanded my skillset through responsibilities <br></br>
+            including: providing client support for prod projects, optimizing ETL processes<br></br>
+            to reduce data processing time, improving data parsing quality, and independently<br></br>
+            engineering core entity-score that I am the expert for on a current project. </p>
+            <br></br>
             <h5>Data Science Associate - York University</h5>
+            <p> Designed data visualization, regression, and cross-validation projects for a <br></br>Master's course with 20+ students.</p>
+            <br></br>
             <h5>Software Developer Intern - CodeDay Labs</h5>
+            <p> Led the development of a cross-platform React Native app, while mentoring two<br></br> 
+            teammates in an agile workflow. Utilized the react-native-camera library for camera<br></br>
+            access and front-end development. Designed the app logo using Procreate and<br></br>
+            GIMP, and created a Figma prototype for product presentation.</p>
+            <br></br>
             <h5>Senior Assistant - Allan I Carswell Observatory</h5>
+            <p>Led the observatory team during outreach events, managing audiences of 100+<br></br>
+            visitors of various expertise. Acquired sets of astronomical data for research projects<br></br>
+            at the observatory and for graduate students. Analyzed and reduced the acquired<br></br>
+            sets of optical data for observatory research.</p>
             <br></br>
             <h3 className="Projects">Projects:</h3>
             <br></br>
             <h5>ML Researcher - CIBC</h5>
+            <p>Directing a thesis project exploring machine learning algorithms optimized for<br></br> 
+            predictive fraud analytics. Goals involve detailed data mining, DQA, and applications<br></br>
+            of statistical and ML algorithms. Will be using my analysis to compose a thesis<br></br> 
+            summarizing my findings, with the potential for the research paper to be published<br></br>
+            in a journal.</p>
+            <br></br>
             <h5>Very-Unique-Pet - 3rd place overall Hacklarious</h5>
+            <p>Conducted the development of an alarm clock with an interactive virtual pet and<br></br>
+            web-app interface that allows control of the alarm clock. Set-up DNS records using<br></br>
+            Google Cloud Platform and established a pub/sub server using Python.</p>
+            <br></br>
             <h5>Ideation Challenge - KPMG</h5>
+            <p>Collaborated within a team to design a product and deliver a pitch to a panel of<br></br>
+            judges within 12 hours. Devised an Outlook extension using Machine Learning and<br></br> 
+            Natural Language Processing to automate time-blocking.</p>
           </div>
           <div className="col-md-5">
             <h3 className="skills">Technical Skills:</h3>
